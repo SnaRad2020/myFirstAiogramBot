ea1680e3ef4c0df175b626d156778c84dfefbc2e
Nikita <gorobtsov9982@gmail.com>
2023-08-21 17:45:14 +0500

    aiogram

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ main_admin.add("Наши песни").add("Контакты").add("Админ-п
 admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
 admin_panel.add("Сделать рассылку").add("Добавить песню").add("Удалить песню").add("Назад")
 
-
+#Список песен
 song_list = InlineKeyboardMarkup(row_width=1)
 song_list.add(
-    InlineKeyboardButton(text='Бананафилы', callback_data='song_bananafils'),
-    InlineKeyboardButton(text='Пёс Барбос', callback_data='song_dog_barbos'),
-    InlineKeyboardButton(text='Каждый день', callback_data='song_every_day')
+    InlineKeyboardButton(text='Бананафилы', url='https://www.youtube.com/watch?v=K7fWE2dDsRE&ab'),
+    InlineKeyboardButton(text='Пёс Барбос', url='https://www.youtube.com/watch?v=PFsMwaeV8Mk&ab'),
+    InlineKeyboardButton(text='Каждый день', url='https://www.youtube.com/watch?v=6h2fVZ70ytI&ab')
 )
 
 
